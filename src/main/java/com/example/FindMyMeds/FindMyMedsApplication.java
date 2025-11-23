115bd2cbc3093f0757450f8c2e52946a4c5ba047
METHU25 <yohanjason25@gmail.com>
2025-11-23 21:00:25 +0530

    main files

---
+++ src/main/java/com/example/FindMyMeds/FindMyMedsApplication.java
@@ -0,0 +1,13 @@
+package com.example.FindMyMeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyMedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyMedsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FindMyMeds/FindMyMedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FindMyMeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyMedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
