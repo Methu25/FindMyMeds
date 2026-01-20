0eb18850f2cb41ea3d0415507b3876771b8eae71
Rakshan200417 <kukarakshan2004@gmail.com>
2026-01-20 11:35:30 +0530

    new edited files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2FA4A9',
+        'primary-dark': '#27888C',
+      }
+    },
+  },
+  plugins: [],
+}
