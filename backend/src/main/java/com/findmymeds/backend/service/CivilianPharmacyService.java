e330dba780184543bd471aeaf70e3127f6cf6da5
P.Rejishanth <rejishanth422@gmail.com>
2026-01-24 12:09:38 +0530

    add pharmacy frontend files & rename some files )

--- backend/src/main/java/com/findmymeds/backend/service/CivilianPharmacyService.java
+++ backend/src/main/java/com/findmymeds/backend/service/CivilianPharmacyService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class PharmacyService {
+public class CivilianPharmacyService {
 
     @Autowired
     private PharmacyRepository pharmacyRepository;
