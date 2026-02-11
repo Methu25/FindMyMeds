fc61321582f5414abf573e0239332738748feabd
Gaweesha <gaweesha2003@gmail.com>
2026-02-11 18:30:12 +0530

    Resolved merge conflict in ReservationItemDTO

--- backend/src/main/java/com/findmymeds/backend/dto/ReservationItemDTO.java
+++ backend/src/main/java/com/findmymeds/backend/dto/ReservationItemDTO.java
@@ -4,10 +4,9 @@ import lombok.Data;
 
 @Data
 public class ReservationItemDTO {
+
     private Long id;
-    private String medicineName;
+    private MedicineDTO medicine;
     private int quantity;
     private Double price;
-    private Double subtotal;
-    private MedicineDTO medicine;
 }
